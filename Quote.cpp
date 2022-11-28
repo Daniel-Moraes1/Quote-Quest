50f670f3cc8c9d5f5a044a434462efcc42944845
Alvin Bang <alvin.bang@ufl.edu>
2022-11-28 00:12:38 -0500

    Create Quote.cpp

---
+++ Quote.cpp
@@ -0,0 +1,46 @@
+#include "Quote.h"
+#include <string>
+
+Quote::Quote()
+{
+	this->sentimentVal = 0;
+	this->length = 0;
+	this->quote = "";
+	this->category = "";
+	this->author = "";
+}
+
+Quote::Quote(int sentimentVal, int length, std::string quote, std::string category, std::string author)
+{
+	this->sentimentVal = sentimentVal;
+	this->length = length;
+	this->quote = quote;
+	this->category = category;
+	this->author = author;
+}
+
+int Quote::getSentimentVal()
+{
+	return this->sentimentVal;
+}
+
+int Quote::getLength()
+{
+	return this->length;
+}
+
+std::string Quote::getQuote()
+{
+	return this->quote;
+}
+
+std::string Quote::getCategory()
+{
+	return this->category;
+}
+
+
+std::string Quote::getAuthor()
+{
+	return this->author;
+}
