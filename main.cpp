0967acb4b91d98214de72810e248d0087b7bc4f7
Alvin Bang <alvin.bang@ufl.edu>
2022-12-05 20:46:38 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <string>
 #include <iostream>
+#include <iomanip>
 #include <fstream>
 #include <sstream>
 #include "Quote.h"
@@ -18,7 +19,7 @@ class AdjacencyList
 
 void AdjacencyList::insertData()
 {
-	std::ifstream inFile("data.csv");
+	std::ifstream inFile("test_data.csv");
 	std::string num;
 	std::string quote;
 	std::string author;
@@ -29,17 +30,47 @@ void AdjacencyList::insertData()
 	if (inFile.is_open())
 	{
 		std::string line;
+		std::string temp;
 		getline(inFile, line);
 
 		while (getline(inFile, line))
 		{
 			std::istringstream stream(line);
 			getline(stream, num, ',');
-			getline(stream, quote, ',');
-			getline(stream, author, ',');
-			getline(stream, category, ',');
-			getline(stream, sentimentVal, ',');
 			getline(stream, length, ',');
+			getline(stream, sentimentVal, ',');
+			getline(stream, temp);
+			if (temp[0] == '"')
+			{
+				quote = temp.substr(1, std::stoi(length));
+				temp = temp.substr(temp.find("\",") + 2);
+			}
+			else
+			{
+				quote = temp.substr(0, std::stoi(length));
+				temp = temp.substr(temp.find(',') + 1);
+			}
+
+			if (temp[0] == '"')
+			{
+				author = temp.substr(1, temp.find("\",") - 1);
+				temp = temp.substr(temp.find("\",") + 2);
+			}
+			else
+			{
+				author = temp.substr(0, temp.find(','));
+				temp = temp.substr(temp.find(',') + 1);
+			}
+
+			if (temp[0] == '"')
+			{
+				category = temp.substr(1, temp.length() - 2);
+			}
+			else
+			{
+				category = temp.substr(0, temp.find(','));
+			}
+			Quote q(std::stod(sentimentVal), std::stoi(length), quote, category, author);
 		}
 	}
 }
